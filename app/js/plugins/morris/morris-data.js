b72136ce36f2d8001f64b27b60900c61a96656ee
krolik <markrolikowski@gmail.com>
2016-05-07 17:36:22 +0200

    I mamy EURO

--- app/js/plugins/morris/morris-data.js
+++ app/js/plugins/morris/morris-data.js
@@ -2,31 +2,31 @@
 
 $(function() {
     var data = [
-
-
-
-
     {
         period: '2016 Q1',
-        usd: 4.23,
-        gbp: 2.35,
-        yen: 4.11
+        usd: 3.99,
+        eur: 4.20,
+        gbp: 6.35,
+        yen: 1.11
     },{
         period: '2016 Q2',
-        usd: 4.23,
-        gbp: 2.35,
-        yen: 4.11
+        usd: 4.02,
+        eur: 4.30,
+        gbp: 6.00,
+        yen: 1.20
     },{
         period: '2016 Q3',
-        usd: 1.23,
-        gbp: 2.35,
-        yen: 4.11
+        usd: 4.20,
+        eur: 4.40,
+        gbp: 6.15,
+        yen: 1.50
     },
         {
         period: '2016 Q4',
-        usd: 4.23,
-        gbp: 5.35,
-        yen: 4.11
+        usd: 4.10,
+        eur: 4.50,
+        gbp: 6.60,
+        yen: 1.80
     },
         /*
 
